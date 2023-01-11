13b06c5702b158655b269ad67c0616e8140c4d3a
Bas Wind <bas@baswind.nl>
2023-01-11 21:25:00 +0100

    Use unwrap_or_else closure to defer unwrapping of r1

--- src/calc.rs
+++ src/calc.rs
@@ -7,16 +7,16 @@
 //! and a 4-item crop tuple.
 
 #[derive(Debug)]
-struct ValidationErr;
+pub struct ValidationErr;
 
 // x, y point expressed in pixels
-struct Point {
+pub struct Point {
     x: u32,
     y: u32,
 }
 
 // x, y point expressed in percentages
-struct RelativePoint {
+pub struct RelativePoint {
     x: f64,
     y: f64,
 }
@@ -31,22 +31,30 @@ impl RelativePoint {
         }
         Ok(Self { x, y })
     }
+
+    pub fn x(&self) -> f64 {
+        self.x
+    }
+
+    pub fn y(&self) -> f64 {
+        self.y
+    }
 }
 
 #[derive(Debug, PartialEq)]
-struct Box {
+pub struct Box {
     w: u32,
     h: u32,
 }
 
 impl Box {
-    fn floats(&self) -> (f64, f64) {
+    pub fn floats(&self) -> (f64, f64) {
         (self.w as f64, self.h as f64)
     }
 }
 
 // A Box that may have at most one missing value
-struct OptionBox {
+pub struct OptionBox {
     w: Option<u32>,
     h: Option<u32>,
 }
@@ -58,10 +66,18 @@ impl OptionBox {
         }
         Err(ValidationErr)
     }
+
+    pub fn w(&self) -> Option<u32> {
+        self.w
+    }
+
+    pub fn h(&self) -> Option<u32> {
+        self.h
+    }
 }
 
 #[derive(Debug, PartialEq)]
-struct CropBox {
+pub struct CropBox {
     top: u32,
     left: u32,
     right: u32,
@@ -127,14 +143,11 @@ fn crop_box(image_box: &Box, crop_box: &Box, focal_point: &RelativePoint) -> Cro
 // the aspect ratio of `image_box` and the length of the edge in `resize_box`.
 fn add_missing_edge(image_box: &Box, resize_box: &OptionBox) -> Box {
     let (iw, ih) = image_box.floats();
-    let (rw, rh) = (
-        resize_box.w.unwrap_or_default() as f64,
-        resize_box.h.unwrap_or_default() as f64,
-    );
-    let calc_edge =
-        |i1: f64, i2: f64, r1: f64, r2: Option<u32>| r2.unwrap_or(((i1 / i2) * r1) as u32);
-    let w = calc_edge(iw, ih, rh, resize_box.w);
-    let h = calc_edge(ih, iw, rw, resize_box.h);
+    let calc_edge = |i1: f64, i2: f64, r1: Option<u32>, r2: Option<u32>| {
+        r2.unwrap_or_else(|| ((i1 / i2) * (r1.unwrap() as f64)) as u32)
+    };
+    let w = calc_edge(iw, ih, resize_box.h, resize_box.w);
+    let h = calc_edge(ih, iw, resize_box.w, resize_box.h);
     Box { w, h }
 }
 
@@ -161,7 +174,7 @@ fn crop_and_zoom(image_box: &Box, crop_box: &Box, zoom: &Option<f64>) -> Box {
 }
 
 // Fits, then crops.
-fn fit(
+pub fn fit(
     image_box: &Box,
     resize_box: &OptionBox,
     focal_point: &RelativePoint,
@@ -174,7 +187,7 @@ fn fit(
 }
 
 // Crop resizes, then crops.
-fn crop(
+pub fn crop(
     image_box: &Box,
     resize_box: &Box,
     focal_point: &RelativePoint,
@@ -183,4 +196,372 @@ fn crop(
     let resized_and_zoomed = crop_and_zoom(&image_box, &resize_box, &zoom);
     let cropped = crop_box(&resized_and_zoomed, &resize_box, &focal_point);
     (resized_and_zoomed, cropped)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_option_box_is_ok() {
+        assert!(OptionBox::build(Some(100), Some(200)).is_ok());
+        assert!(OptionBox::build(None, Some(200)).is_ok());
+        assert!(OptionBox::build(Some(100), None).is_ok());
+    }
+
+    #[test]
+    fn test_option_box_is_err() {
+        assert!(OptionBox::build(None, None).is_err());
+    }
+
+    #[test]
+    fn test_relative_point_validates() {
+        assert!(RelativePoint::build(0., 100.).is_ok());
+    }
+
+    #[test]
+    fn test_relative_point_does_not_validate() {
+        assert!(RelativePoint::build(0., 100.1).is_err());
+        assert!(RelativePoint::build(-0.1, 100.).is_err());
+    }
+
+    #[test]
+    fn test_true_focal_point_rel() {
+        assert_eq!(
+            true_focal_point_rel(0., 8.333333333333334),
+            41.6666666666666664,
+        );
+    }
+
+    #[test]
+    fn test_true_focal_point_out_of_bounds_upper() {
+        assert_eq!(true_focal_point(1536, 1280, 100.0), 896);
+    }
+
+    #[test]
+    fn test_true_focal_point_out_of_bounds_lower() {
+        assert_eq!(true_focal_point(1536, 1280, 0.), 640);
+    }
+
+    #[test]
+    fn test_true_focal_point_middle() {
+        assert_eq!(true_focal_point(1536, 1280, 50.), 768);
+    }
+
+    #[test]
+    fn test_true_focal_point_crop_is_larger_than_image_centers_focal_point() {
+        assert_eq!(true_focal_point(1024, 1280, 100.), 512);
+    }
+
+    #[test]
+    fn test_get_space() {
+        assert_eq!(get_space(1536, 1280), 8.333333333333334);
+    }
+
+    #[test]
+    fn test_crop_box_out_of_bounds_upper() {
+        assert_eq!(
+            crop_box(
+                &Box { w: 1536, h: 1152 },
+                &Box { w: 1280, h: 720 },
+                &RelativePoint { x: 100., y: 100. }
+            ),
+            CropBox {
+                top: 256,
+                left: 432,
+                bottom: 1536,
+                right: 1152,
+            }
+        );
+    }
+
+    #[test]
+    fn test_crop_box_out_of_bounds_lower() {
+        assert_eq!(
+            crop_box(
+                &Box { w: 1536, h: 1152 },
+                &Box { w: 1280, h: 720 },
+                &RelativePoint { x: 0., y: 0. }
+            ),
+            CropBox {
+                top: 0,
+                left: 0,
+                bottom: 1280,
+                right: 720,
+            }
+        );
+    }
+
+    #[test]
+    fn test_crop_box_middle() {
+        assert_eq!(
+            crop_box(
+                &Box { w: 1536, h: 1152 },
+                &Box { w: 1280, h: 720 },
+                &RelativePoint { x: 50., y: 50. }
+            ),
+            CropBox {
+                top: 128,
+                left: 216,
+                bottom: 1408,
+                right: 936,
+            }
+        );
+    }
+
+    #[test]
+    fn test_add_missing_edge_height() {
+        assert_eq!(
+            add_missing_edge(
+                &Box { w: 100, h: 50 },
+                &OptionBox {
+                    w: Some(50),
+                    h: None
+                }
+            ),
+            Box { w: 50, h: 25 }
+        );
+    }
+
+    #[test]
+    fn test_add_missing_edge_width() {
+        assert_eq!(
+            add_missing_edge(
+                &Box { w: 100, h: 50 },
+                &OptionBox {
+                    w: None,
+                    h: Some(250)
+                }
+            ),
+            Box { w: 500, h: 250 }
+        );
+    }
+
+    #[test]
+    fn test_resize_and_zoom() {
+        assert_eq!(
+            resize_and_zoom(
+                &Box { w: 1920, h: 1440 },
+                &Box { w: 1280, h: 720 },
+                &Some(1.2)
+            ),
+            Box { w: 1152, h: 864 }
+        );
+    }
+
+    #[test]
+    fn test_crop_and_zoom() {
+        assert_eq!(
+            crop_and_zoom(
+                &Box { w: 1920, h: 1440 },
+                &Box { w: 1280, h: 720 },
+                &Some(1.2)
+            ),
+            Box { w: 1536, h: 1152 }
+        );
+    }
+
+    #[test]
+    fn test_fit_same_aspect_ratio() {
+        assert_eq!(
+            fit(
+                &Box { w: 1920, h: 1440 },
+                &OptionBox {
+                    w: Some(640),
+                    h: Some(480)
+                },
+                &RelativePoint { x: 50., y: 50. },
+                &None,
+            ),
+            (
+                Box { w: 640, h: 480 },
+                CropBox {
+                    top: 0,
+                    left: 0,
+                    bottom: 640,
+                    right: 480
+                }
+            )
+        );
+    }
+
+    #[test]
+    fn test_fit_yields_narrower_image() {
+        assert_eq!(
+            fit(
+                &Box { w: 1920, h: 1440 },
+                &OptionBox {
+                    w: Some(1280),
+                    h: Some(720)
+                },
+                &RelativePoint { x: 50., y: 50. },
+                &None
+            ),
+            (
+                Box { w: 960, h: 720 },
+                CropBox {
+                    top: 0,
+                    left: 0,
+                    bottom: 960,
+                    right: 720
+                },
+            )
+        );
+    }
+
+    #[test]
+    fn test_fit_yields_shorter_image() {
+        assert_eq!(
+            fit(
+                &Box { w: 1920, h: 1440 },
+                &OptionBox {
+                    w: Some(1280),
+                    h: Some(1280)
+                },
+                &RelativePoint { x: 50., y: 50. },
+                &None
+            ),
+            (
+                Box { w: 1280, h: 960 },
+                CropBox {
+                    top: 0,
+                    left: 0,
+                    bottom: 1280,
+                    right: 960
+                }
+            )
+        );
+    }
+
+    #[test]
+    fn test_fit_with_zoom_removes_top_and_bottom() {
+        assert_eq!(
+            fit(
+                &Box { w: 1920, h: 1440 },
+                &OptionBox {
+                    w: Some(1280),
+                    h: Some(720)
+                },
+                &RelativePoint { x: 50., y: 50. },
+                &Some(1.2)
+            ),
+            (
+                Box { w: 1152, h: 864 },
+                CropBox {
+                    top: 0,
+                    left: 72,
+                    bottom: 1152,
+                    right: 792
+                }
+            )
+        );
+    }
+
+    #[test]
+    fn test_fit_without_width_calculates_width() {
+        assert_eq!(
+            fit(
+                &Box { w: 1920, h: 1440 },
+                &OptionBox {
+                    w: Some(1280),
+                    h: None
+                },
+                &RelativePoint { x: 50., y: 50. },
+                &None
+            ),
+            (
+                Box { w: 1280, h: 960 },
+                CropBox {
+                    top: 0,
+                    left: 0,
+                    bottom: 1280,
+                    right: 960
+                }
+            )
+        );
+    }
+
+    #[test]
+    fn test_crop_same_aspect_ratio() {
+        assert_eq!(
+            crop(
+                &Box { w: 1920, h: 1440 },
+                &Box { w: 640, h: 480 },
+                &RelativePoint { x: 50., y: 50. },
+                &None
+            ),
+            (
+                Box { w: 640, h: 480 },
+                CropBox {
+                    top: 0,
+                    left: 0,
+                    bottom: 640,
+                    right: 480
+                }
+            )
+        );
+    }
+
+    #[test]
+    fn test_crop_removes_top_and_bottom() {
+        assert_eq!(
+            crop(
+                &Box { w: 1920, h: 1440 },
+                &Box { w: 1280, h: 720 },
+                &RelativePoint { x: 50., y: 50. },
+                &None
+            ),
+            (
+                Box { w: 1280, h: 960 },
+                CropBox {
+                    top: 0,
+                    left: 120,
+                    bottom: 1280,
+                    right: 840
+                }
+            )
+        );
+    }
+
+    #[test]
+    fn test_crop_removes_sides() {
+        assert_eq!(
+            crop(
+                &Box { w: 1920, h: 1440 },
+                &Box { w: 960, h: 960 },
+                &RelativePoint { x: 50., y: 50. },
+                &None,
+            ),
+            (
+                Box { w: 1280, h: 960 },
+                CropBox {
+                    top: 160,
+                    left: 0,
+                    bottom: 1120,
+                    right: 960,
+                },
+            ),
+        );
+    }
+
+    #[test]
+    fn test_crop_with_zoom_removes_sides_and_top_and_bottom() {
+        assert_eq!(
+            crop(
+                &Box { w: 1920, h: 1440 },
+                &Box { w: 1280, h: 720 },
+                &RelativePoint { x: 50., y: 50. },
+                &Some(1.2)
+            ),
+            (
+                Box { w: 1536, h: 1152 },
+                CropBox {
+                    top: 128,
+                    left: 216,
+                    bottom: 1408,
+                    right: 936
+                }
+            )
+        );
+    }
 }
